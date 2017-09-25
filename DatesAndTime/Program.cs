75e10a77a09779c8a04f2deed0a111e5a64637f4
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-25 19:59:04 +0100

    added more timelord variables.

--- DatesAndTime/Program.cs
+++ DatesAndTime/Program.cs
@@ -10,7 +10,24 @@ namespace DatesAndTime
     {
         static void Main(string[] args)
         {
+            DateTime myValue = DateTime.Now;
+            //            Console.WriteLine(myValue.ToString());
+            //            Console.WriteLine(myValue.ToShortDateString());
+            //            Console.WriteLine(myValue.ToShortTimeString());
+            //            Console.WriteLine(myValue.ToLongTimeString());
+            //            Console.WriteLine(myValue.ToLongDateString());
 
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString()); 
+            //Console.WriteLine(myValue.Month);
+
+            //DateTime myBirthday = new DateTime(1522, 2, 4);
+            //Console.WriteLine(myBirthday.ToLongDateString());
+
+
+            DateTime myBirthday = DateTime.Parse("12/07/1888");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalSeconds);
 
             Console.ReadLine();
         }
